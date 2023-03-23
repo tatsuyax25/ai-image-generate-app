775b1134778fc0c38eaeaf2a800da073a5c30203
Miguel Urena <urena_miguel82@outlook.com>
2023-03-23 15:16:14 -0700

    Create button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,8 @@ const App = () => {
         <Link to="/">
           <img src={logo} alt="logo" className='w-28 object-contain' />
         </Link>
+
+        <Link to="/create-post" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">Create</Link>
       </header>
     </BrowserRouter>
   )
